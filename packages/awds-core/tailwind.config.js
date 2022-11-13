73afa11f3bbdac42f7547501c5deb76ee571883b
Luke Bennett <hello@lukebennett.com.au>
2022-11-14 09:01:01 +1100

    Init

---
+++ packages/awds-core/tailwind.config.js
@@ -0,0 +1,39 @@
+function withOpacity(variableName) {
+	return ({ opacityValue }) => {
+		if (opacityValue !== undefined) {
+			return `rgba(var(${variableName}), ${opacityValue})`;
+		}
+		return `rgb(var(${variableName}))`;
+	};
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			aria: {
+				pressed: 'pressed="true"',
+			},
+			textColor: {
+				skin: {
+					base: withOpacity('--color-text-base'),
+					muted: withOpacity('--color-text-muted'),
+					inverted: withOpacity('--color-text-inverted'),
+				},
+			},
+			backgroundColor: {
+				skin: {
+					'fill': withOpacity('--color-fill'),
+					'button-accent': withOpacity('--color-button-accent'),
+					'button-accent-hover': withOpacity('--color-button-accent-hover'),
+					'button-muted': withOpacity('--color-button-muted'),
+				},
+			},
+			ringColor: {
+				'button-accent': withOpacity('--color-button-accent'),
+			},
+		},
+	},
+	plugins: [],
+};
